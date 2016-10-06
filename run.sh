bf23b9df2a1b11029a638da954c7bcd8ab462468
Kefah Issa <kefah.issa@gmail.com>
2016-10-06 09:25:50 -0700

    more stuff

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -x
 #ansible-playbook  -i 'localhost,' -c local --ask-sudo-pass -b playbook.yml $@
-ansible-playbook  -i 'localhost,' -c local -b playbook.yml $@
+ansible-playbook  -i 'localhost,' -c local -b -v playbook.yml $@
