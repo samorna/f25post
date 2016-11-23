77805377c5629d6dfcdd16d8d4195db50f4069c1
Kefah Issa <kefah.issa@gmail.com>
2016-11-23 01:47:08 -0800

    Added arabic fonts shell script for now will make ansible later

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -x
-#ansible-playbook  -i 'localhost,' -c local --ask-sudo-pass -b playbook.yml $@
-ansible-playbook  -i 'localhost,' -c local -b -v playbook.yml $@
+#ansible-playbook  -i 'localhost ansible_become=true,' -c local --ask-sudo-pass -b playbook.yml $@
+ansible-playbook  -i 'localhost ansible_become=true,' -c local -b -v playbook.yml $@
