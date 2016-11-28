6eccd2d7c143b960cca190705a4896f71eaf46e3
Kefah Issa <kefah.issa@gmail.com>
2016-11-27 18:28:37 -0800

    Clean ups and grouped all optional tasks in optional.yml which can be controlled via vars.yml flags

--- run.sh
+++ run.sh
@@ -1,13 +1,12 @@
 #!/bin/bash 
-rpm -qa | grep ansible > /dev/null || sudo dnf install -y --setopt tsflags=nodocs python2 python2-dnf libselinux-python ansible 
+rpm -qa ansible | grep ansible > /dev/null || sudo dnf install -y python2 python2-dnf libselinux-python ansible 
 
 #ansible-playbook  -i 'localhost ansible_become=true,' -c local --ask-sudo-pass -b playbook.yml $@
 sudo ansible-playbook  -i 'localhost ansible_become=true,' -c local -b -v playbook.yml $@
 
+# Should be run as regular user
 gsettings set org.gnome.desktop.interface clock-show-seconds true
 gsettings set org.gnome.desktop.interface clock-show-date true
 gsettings set org.gnome.Terminal.Legacy.Settings default-show-menubar false
-#mkdir ~/.local/share/gnome-shell/extensions
-#cp -a ./files/gnome-extensions/* ~/.local/share/gnome-shell/extensions
 
 
